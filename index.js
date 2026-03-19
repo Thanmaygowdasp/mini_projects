f63124662e7235df854a8269e3a3bbe0abd6877b
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-19 22:44:44 +0530

    added profile

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let menu = document.getElementById("menu")
 let menu_clicker = document.getElementById("menu-clicker")
 let profile_pop = document.getElementById("profile-pop")
 
+//profile_pop.addEventListener("click", function(){
+//    profile_pop.className = "Popped"
+//})
+
 menu_clicker.addEventListener("click", function(){
     menu.classList.toggle("active")
     menu_clicker.style.background =""
