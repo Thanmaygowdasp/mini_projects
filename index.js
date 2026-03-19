efebbf42053babb3622b613d929ccf02584b2be8
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-19 22:18:27 +0530

    profile pop

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let menu = document.getElementById("menu")
 let menu_clicker = document.getElementById("menu-clicker")
+let profile_pop = document.getElementById("profile-pop")
 
 menu_clicker.addEventListener("click", function(){
     menu.classList.toggle("active")
