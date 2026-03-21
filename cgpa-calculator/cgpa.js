093452a35d9ae9f0bdc4b0ac601fc4303f835d80
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-22 01:03:41 +0530

    working on cgpa project

--- cgpa-calculator/cgpa.js
+++ cgpa-calculator/cgpa.js
@@ -1,17 +1,4 @@
-let sub_add = document.getElementById("subject-add")
-sub_add.addEventListener("click", function(){
-    inputadded()
-})
-let maxsubjects = 8
-function inputadded(){
+function addsubjects(){
     let container = document.getElementById("subjects")
-    let inputs = document.querySelectorAll("input")
-    if(inputs.length >= maxsubjects){
-        alert("Are You Sure Adding more Subjects")
-        return
-    }
-    let input = document.createElement("input")
-    input.type = "number"
-    input.placeholder = "Enter your Grade"
-    container.appendChild(input)
+    
 }
