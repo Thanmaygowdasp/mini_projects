61656f1f6534cc3d11b3f5d460bb5147f94be762
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-22 01:39:22 +0530

    limit fixed

--- cgpa-calculator/cgpa.js
+++ cgpa-calculator/cgpa.js
@@ -1,4 +1,29 @@
+let subject_adding = document.getElementById("subject_adding")
+let container = document.getElementById("subjects")
+
+subject_adding.addEventListener("click", function(){
+    addsubjects()
+})
+
+
+
+
+
+// Subject Adding
+let maxsubject = 8
+let sub_count = 0
+let warning = false
 function addsubjects(){
-    let container = document.getElementById("subjects")
-    
+    let inputs = document.createElement("input")
+        inputs.type = "number"
+        inputs.placeholder = "Enter the Grade"
+        container.appendChild(inputs)
+        sub_count++
+
+        if (sub_count>maxsubject && !warning){
+            alert("Warning")
+            warning = true
+        }
 }
+
+
