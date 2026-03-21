1e1b344e84bb34a6c17d1c1ae6b1c288e60d5e3a
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-21 19:36:44 +0530

    profile pop finish

--- index.js
+++ index.js
@@ -3,26 +3,21 @@ let menu_clicker = document.getElementById("menu-clicker")
 let profile_btn = document.getElementById("profile_btn")
 let profile_pop = document.getElementById("profile_pop")
 let dropdown = document.getElementById("dropdown")
+let overlay = document.getElementById("overlay")
+let profile_closer = document.getElementById("profile_closer")
 let isOpen = false
-menu_clicker.addEventListener("click", function(){
-    if(isOpen){
-        dropdown.style.opacity = "0"
-        dropdown.style.marginTop = "-20px"
-        isOpen = false
-    } else {
+menu_clicker.addEventListener("mouseover", function(){
         dropdown.style.opacity = "1"
         dropdown.style.marginTop = "0"
-        isOpen = true
-    }
 })
-let profileopen = false
 profile_btn.addEventListener("click", function(){
-    if(profileopen == false){
-        profile_pop.style.display = "block"
-        profileopen = true
-    }else{
-        profile_pop.style.display = "none"
-        profileopen = false
-    }
+    profile_pop.style.display = "block"
+    overlay.classList.toggle("active")
+})
+profile_closer.addEventListener("click", function(){
+    profile_pop.style.display = "none"
+    overlay.classList.toggle("active")
+    dropdown.style.opacity = "0"
+    dropdown.style.marginTop = "-20px"
 })
 
