9e67a25b629fe174cb9f5b4ba210d74fd9d94c88
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-21 12:33:09 +0530

    js updated

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let menu = document.getElementById("menu")
 let menu_clicker = document.getElementById("menu-clicker")
-let profile_pop = document.getElementById("profile-pop")
-
-//profile_pop.addEventListener("click", function(){
-//    profile_pop.className = "Popped"
-//})
-
+let profile_btn = document.getElementById("profile_btn")
+let profile_pop = document.getElementById("profile_pop")
+let dropdown = document.getElementById("dropdown")
+let isOpen = false
 menu_clicker.addEventListener("click", function(){
-    menu.classList.toggle("active")
-    menu_clicker.style.background =""
+    if(isOpen){
+        dropdown.style.opacity = "0"
+        dropdown.style.marginTop = "-20px"
+        isOpen = false
+    } else {
+        dropdown.style.opacity = "1"
+        dropdown.style.marginTop = "0"
+        isOpen = true
+    }
 })
+let profileopen = false
+profile_btn.addEventListener("click", function(){
+    if(profileopen == false){
+        profile_pop.style.display = "block"
+        profileopen = true
+    }else{
+        profile_pop.style.display = "none"
+        profileopen = false
+    }
+})
+
