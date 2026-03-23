e9570482b364af996af9d69e14e33c99d99ce469
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-23 11:40:08 +0530

    cgpa update

--- cgpa-calculator/cgpa.js
+++ cgpa-calculator/cgpa.js
@@ -1,48 +1,47 @@
+let subjects = document.getElementById("subjects")
 let subject_adding = document.getElementById("subject_adding")
-let container = document.getElementById("subjects")
-let calculator = document.getElementById("calculator")
+let cgpacalculate = document.getElementById("calculator")
 
 subject_adding.addEventListener("click", function(){
-    addsubjects()
+    subject_add()
 })
 
-calculator.addEventListener("click", function(){
+cgpacalculate.addEventListener("click", function(){
     cgpacalculator()
 })
 
-// Subject Adding
-let maxsubject = 8
-let sub_count = 0
-let warning = false
-function addsubjects(){
-    let inputs = document.createElement("input")
-        inputs.type = "number"
-        inputs.placeholder = "Enter the Grade"
-        container.appendChild(inputs)
-        sub_count++
+let waringsubjects = 8
+let subjects_added = 0
+let alretmsgforsub = false
+function subject_add(){
+    let container = document.getElementById("subjects")
+    let input = document.createElement("input")
 
-        if (sub_count>maxsubject && !warning){
-            alert("Warning")
-            warning = true
+    input.type = "Number"
+    input.placeholder = "Enter the Grade"
+
+    container.append(input)
+    subjects_added++
+
+    if(subjects_added > waringsubjects){
+        if(!alretmsgforsub){
+            alert("Waring")
+            alretmsgforsub = true
         }
+    }
 }
 
 function cgpacalculator(){
-    let inputnum = document.querySelectorAll("#subjects input")
+    let inputs = document.querySelectorAll("#subjects input")
 
-    let total_cgpa = 0
-    let count_cgpa = 0
+    let total_count = 0
+    let subject_count = 0
 
-    inputnum.forEach(function(input){
-        let value = Number(input.value)
+    let value = Number(inputs.value)
 
-        if(input.value !== "" && value <= 10 && value >= 0){
-            total_cgpa = total_cgpa + value
-            count_cgpa++
+    inputs.forEach(function(input){
+        if(inputs.value != ""){
+            console.log(value)
         }
     })
-    console.log("total:", total_cgpa)
-    console.log("count:", count_cgpa)
-    let cgpa = total_cgpa / count_cgpa
-    console.log(cgpa)
 }
