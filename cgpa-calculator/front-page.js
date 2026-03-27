23175190c02d742062973276eda267ee37f6e898
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-03-27 12:38:58 +0530

    sgpa calculation starting

--- cgpa-calculator/front-page.js
+++ cgpa-calculator/front-page.js
@@ -1,6 +1,9 @@
 let sgpa_section = document.getElementById("sgpa-section")
 let opensgpa = document.getElementById("opensgpa")
 let backhome = document.getElementById("backhome")
+let subjectinputs = document.getElementById("subjectInputs")
+let subjectcount = document.getElementById("subjectcount")
+let addExtra = document.getElementById("addExtra")
 
 opensgpa.addEventListener("click", function(){
     sgpa_section.classList.add("show")
@@ -9,3 +12,28 @@ opensgpa.addEventListener("click", function(){
 backhome.addEventListener("click", function(){
     sgpa_section.classList.remove("show")
 })
+
+
+function createinput(){
+    let div = document.createElement("div")
+    div.innerHTML = `
+    <input type="number" placeholder="enter Your Marks">
+    <input type="number" placeholder="enter Your credits">
+    `;
+    subjectinputs.appendChild(div)
+}
+
+subjectcount.onchange = function(){
+    subjectinputs.innerHTML = ""
+    let count = Number(this.value)
+    for(let i = 0; i< count; i++){
+        createinput()
+    }
+}
+
+addExtra.addEventListener("click", function(){
+    createinput()
+})
+
+console.log("Dropdown working:", this.value)
+
