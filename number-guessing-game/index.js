b8e2d07ba3c9333e6c317bd83a393db9cd260151
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-04-04 22:38:15 +0530

    updated

--- number-guessing-game/index.js
+++ number-guessing-game/index.js
@@ -1,122 +1,65 @@
-let maxattempt = document.getElementById("Maxattempt")
-let maxchancebtn = document.getElementById("maxchancebtn")
-let userattempt = document.getElementById("userattempt")
-let userattemptasked = document.getElementById("userattemptasked") 
-let computergenerated = document.getElementById("computergenerated")
-let generatebtn = document.getElementById("generatebtn")
-let userguessingnumber = document.getElementById("userguessingnumber")
-let userguessingbtn = document.getElementById("userguessingbtn")
-let userguessed = document.getElementById("userguessed")
-let reset = document.getElementById("resetbtn")
-let difference = document.getElementById("difference")
-let winner = document.getElementById("winner")
+const btn = document.getElementById("hoverBtn");
+const leftNum = document.getElementById("leftNum");
+const rightNum = document.getElementById("rightNum");
+const userinput = document.getElementById("userinput")
+const submit = document.getElementById("submitBtn")
+const Warning = document.getElementById("Warning")
 
-// user chances
-let user_asked_chance = 0
-let attempts = 0
-maxchancebtn.addEventListener("click", function(){
-    user_asked_chance = Number(maxattempt.value)
-    attempts = user_asked_chance
-    userattemptasked.textContent = attempts
-})
+let leftInterval;
+let rightInterval;
 
-//computer generated
-let generated_number = 0
-generatebtn.addEventListener("click", function(){
-    generated_number = Math.floor(Math.random()*100)+1
-    computergenerated.textContent = "Generated"
-    if(attempts === 0){
-        generatebtn.disabled = true
-        userguessingbtn.disabled = true
-        alert("Please select Your Chances First! please Refresh and play")
-    }
-})
+function animateNumber(element, target, side) {
+    let count = 0;
 
-// user number
-let user_guessing_number = 0
-userguessingbtn.addEventListener("click", function(){
+    const interval = setInterval(() => {
+        count++;
+        element.textContent = count;
 
-    if(attempts==0){
-        userattemptasked.textContent = "Sorry Try Again"
-        return 0
-    }
-    user_guessing_number = Number(userguessingnumber.value)
-    userguessed.textContent = user_guessing_number
-
-    findingdifference()
-
-    attempts--
-    userattemptasked.textContent = attempts
-
-    if(attempts == 0 && user_guessing_number!=generated_number){
-        difference.textContent = "You Failed"
-        userattemptasked.textContent = "Sorry Try Again"
-        userguessingbtn.disabled = true
-        generatebtn.disabled = true
-    }
-
-    if(user_guessing_number == generated_number){
-        userattemptasked.textContent = "You Won! Try Again By resetting"
-        userguessingbtn.disabled = true
-        generatebtn.disabled = true
-    }
-})
-
-function findingdifference(){
-
-    let subtracted = Math.abs(user_guessing_number - generated_number)
-
-    if(user_guessing_number>generated_number){
-        difference.textContent = "Your Far From = " + subtracted
-    }else if(generated_number>user_guessing_number){
-        difference.textContent = "Your Far From = " + subtracted
-    }else{
-        difference.textContent = ""
-        winner.textContent = "You Won The Game"
-        userattemptasked.textContent = "You Won"
-        computergenerated.textContent = generated_number
-        celebrateWin()
-    }
-}
-
-// reset part
-reset.addEventListener("click", function(){
-    attempts = 0
-    user_asked_chance = 0
-    generatebtn.disabled = false
-    generated_number = 0
-    userguessingbtn.disabled = false
-    computergenerated.textContent = ""
-    user_guessing_number = 0
-    userguessed.textContent = ""
-    difference.textContent = ""
-    winner.textContent = ""
-    userattemptasked.textContent = ""
-})
-
-function celebrateWin() {
-
-    var duration = 2000;
-    var end = Date.now() + duration;
-
-    (function frame() {
-        confetti({
-            particleCount: 5,
-            angle: 60,
-            spread: 55,
-            origin: { x: 0.3, y:1 }
-        });
-
-        confetti({
-            particleCount: 5,
-            angle: 120,
-            spread: 55,
-            origin: { x: 0.7, y: 1 }
-        });
-
-        if (Date.now() < end) {
-            requestAnimationFrame(frame);
+        if (count >= target) {
+            clearInterval(interval);
         }
-    })();
+    }, 15);
+
+    if (side === "left") leftInterval = interval;
+    if (side === "right") rightInterval = interval;
 }
 
+btn.addEventListener("mouseenter", () => {
+    animateNumber(leftNum, 100, "left");
+    animateNumber(rightNum, 100, "right");
+});
+
+btn.addEventListener("mouseleave", () => {
+    clearInterval(leftInterval);
+    clearInterval(rightInterval);
+
+    leftNum.textContent = "";
+    rightNum.textContent = "";
+});
+let generated_number = 0
+btn.addEventListener("click", ()=>{
+    generated_number = Math.floor(Math.random()*100)+1
+    console.log(generated_number)
+})
+let chances = 3
+function guessing_answer(){
+    if(chances>0){
+        if(user_enter_number == generated_number){
+            console.log("Done")
+            Warning.innerText = "You Won Bro"
+        }else{
+            console.log("none")
+        }
+        chances = chances - 1
+        Warning.innerText = `You Left With ${chances} chances`
+        if(chances == 0){
+            submit.style.display = "none"
+            Warning.innerText = "Please Reset Bro"
+        }
+    }
+}
+let user_enter_number = 0
+submit.addEventListener("click", ()=>{
+    user_enter_number = Number(userinput.value)
+    guessing_answer()
+})
