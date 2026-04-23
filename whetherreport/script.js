9bc01e56782bd9c5e9ad265984973d81739d1249
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-04-23 23:43:13 +0530

    outer layer

--- whetherreport/script.js
+++ whetherreport/script.js
@@ -1,12 +1,29 @@
 const apikey = "0e2c3ed3a3fb052de4a1cbb724359ea2"
 const cityname = document.getElementById("city")
+const temp = document.getElementById("temp")
+const humidity = document.getElementById("humidity")
+const loading = document.getElementById("loading")
+const icons = document.getElementById("icon")
 
 async function getwhether() {
     const city = document.getElementById("Cityname").value
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
-   const response = await fetch(url)
-   const data = await response.json()
-   console.log(data.main.temp)
-   cityname.innerHTML = data.main.city
-}
 
+    loading.innerHTML = "Loading"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data.weather[0].icon)
+
+    loading.innerHTML = ""
+    if(data.cod !== 200){
+        cityname.innerHTML = "CITY NOT CORRECT"
+        temp.innerHTML = ""
+        humidity.innerHTML = ""
+        return
+    }
+    cityname.innerHTML = data.name
+    temp.innerHTML = data.main.temp + " °C"
+    humidity.innerHTML = data.main.humidity + " %"
+    const iconcode = data.weather[0].icon
+    icons.src = `https://openweathermap.org/img/wn/${iconcode}@2x.png` 
+}
