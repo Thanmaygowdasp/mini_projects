90c7d8476cfcb27c837465d8dcbd1986a34f1da2
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-04-24 23:31:36 +0530

    forecast

--- whetherreport/script.js
+++ whetherreport/script.js
@@ -9,10 +9,19 @@ async function getwhether() {
     const city = document.getElementById("Cityname").value
 
     loading.innerHTML = "Loading"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+    const geourl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`
+    const georesponse = await fetch(geourl)
+    const geodata = await georesponse.json()
+    let lat = geodata[0].lat
+    let lon = geodata[0].lon
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data.weather[0].icon)
+    const forcasturl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`
+    const foreresponse = await fetch(forcasturl)
+    const foredata = await foreresponse.json()
+    console.log(foredata)
+    console.log(data)
 
     loading.innerHTML = ""
     if(data.cod !== 200){
