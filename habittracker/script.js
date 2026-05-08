3fe9440b444908819b720b1afe4a4ddd43a1d4a0
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-05-08 22:07:18 +0530

    new website

---
+++ habittracker/script.js
@@ -0,0 +1,7 @@
+const habitadderbtn = document.getElementById("habitadder")
+const infogather = document.getElementById("infogather")
+
+habitadderbtn.addEventListener("click", ()=>{
+    infogather.innerHTML = `
+    `
+})
